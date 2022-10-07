7f0f6cf2b054a208302fe45e710e7b46fa3b17f2
ITvladislavovna <irichka-k@rambler.ru>
2022-10-07 09:26:38 +0300

    card order

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/qhf7iatacf7u94o4/branch/master?svg=true)](https://ci.appveyor.com/project/Irina72732/cardorder/branch/master)
---
+++ src/test/java/ru/netology/web/CardTest.java
@@ -0,0 +1,51 @@
+package ru.netology.web;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+
+
+public class CardTest {
+    private WebDriver driver;
+
+
+
+    @BeforeAll
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(true);
+        driver = new ChromeDriver(options);
+          }
+
+    @AfterEach
+    void tearsDown() {
+        driver.quit();
+        driver=null;
+    }
+
+    @Test
+    void shouldTest () throws InterruptedException {
+       driver.get("http://localhost:9999/");
+       driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василий");
+       driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79023278228");
+       driver.findElement(By.className("checkbox__box")).click();
+       driver.findElement(By.tagName("button")).click();
+       String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+       String actual = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText().trim();
+       Assertions.assertEquals(expected,actual);
+    }
+}
+
+
